953a544ba3e456fb658eb098f49bf313525de26e
Randall Ferree <Rander510@aol.com>
2012-05-07 22:44:20 -0500

    Added logging: NOT IMPLEMENTED

---
+++ src/main/java/com/randude14/hungergames/Logging.java
@@ -0,0 +1,30 @@
+package com.randude14.hungergames;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Logging {
+	private static final Logger logger = Logger.getLogger("MyHungerGames");
+
+	public static void log(Level level, String record) {
+		logger.log(level, record);
+	}
+
+	static {
+		try {
+			Plugin instance = Plugin.getInstance();
+			instance.getDataFolder().mkdirs();
+			File file = new File(instance.getDataFolder(), "myhungergames.log");
+			if (!file.exists())
+				file.createNewFile();
+			FileHandler handler = new FileHandler("plugins/MyHungerGames/myhungergames.log", true);
+			logger.addHandler(handler);
+		} catch (IOException ex) {
+		}
+
+	}
+
+}
