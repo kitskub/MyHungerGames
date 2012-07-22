9d2001f2803880a8fd93563287d852d76a6b9259
Jack Huey <kitskub@gmail.com>
2012-07-22 17:26:45 -0500

    Fix chat

--- src/main/java/com/randude14/hungergames/listeners/ChatListener.java
+++ src/main/java/com/randude14/hungergames/listeners/ChatListener.java
@@ -6,6 +6,7 @@ import com.randude14.hungergames.GameManager;
 import com.randude14.hungergames.HungerGames;
 import com.randude14.hungergames.Logging;
 import com.randude14.hungergames.games.HungerGame;
+import java.util.HashSet;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,38 +19,36 @@ public class ChatListener implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void playerChat(PlayerChatEvent event) {
 		HungerGame chatterGame = GameManager.getSession(event.getPlayer());
-		for (Player p : event.getRecipients()) {
+		for (Player p : new HashSet<Player>(event.getRecipients())) {
 			HungerGame receipientGame = GameManager.getSession(event.getPlayer());
-			if (receipientGame != null) {
-				if (Config.getIsolatePlayerChat(receipientGame.getSetup())) {
-					if (chatterGame != null) {
-						if (chatterGame.compareTo(receipientGame) == 0 
-							&& event.getPlayer().getLocation().getWorld() == p.getLocation().getWorld()) {
-							float distanceRequired = Config.getChatDistance(receipientGame.getSetup());
-							if (distanceRequired != 0 && event.getPlayer().getLocation().distance(p.getLocation()) >= distanceRequired) {
-								if (HungerGames.checkPermission(event.getPlayer(), Perm.ADMIN_STOP)) {
-									if (event.getMessage().startsWith("hg ")) {
-										event.setMessage(event.getMessage().substring(3));
-										return;
-									}
-									if (event.getMessage().startsWith("hg")) {
-										event.setMessage(event.getMessage().substring(2));
-										return;
-									}
+			if (receipientGame != null && Config.getIsolatePlayerChat(receipientGame.getSetup())) {
+				if (chatterGame != null) {
+					if (chatterGame.compareTo(receipientGame) == 0 
+						&& event.getPlayer().getLocation().getWorld() == p.getLocation().getWorld()) {
+						float distanceRequired = Config.getChatDistance(receipientGame.getSetup());
+						if (distanceRequired != 0 && event.getPlayer().getLocation().distance(p.getLocation()) >= distanceRequired) {
+							if (HungerGames.checkPermission(event.getPlayer(), Perm.ADMIN_STOP)) {
+								if (event.getMessage().startsWith("hg ")) {
+									event.setMessage(event.getMessage().substring(3));
+									return;
 								}
-								//Logging.debug("Cancelling chat because too far.");
-								event.setCancelled(true);
+								if (event.getMessage().startsWith("hg")) {
+									event.setMessage(event.getMessage().substring(2));
+									return;
+								}
 							}
-						}
-						else {
-							//Logging.debug("Cancelling chat because games are not the same or different worlds.");
-							event.setCancelled(true);
+							//Logging.debug("Cancelling chat because too far.");
+							event.getRecipients().remove(p);
 						}
 					}
 					else {
-						//Logging.debug("Cancelling chat because chatter was not in the gamer.");
-						event.setCancelled(true);
+						//Logging.debug("Cancelling chat because games are not the same or different worlds.");
+						event.getRecipients().remove(p);
 					}
+				}
+				else {
+					//Logging.debug("Cancelling chat because chatter was not in a game.");
+					event.getRecipients().remove(p);
 				}
 			}
 		}
