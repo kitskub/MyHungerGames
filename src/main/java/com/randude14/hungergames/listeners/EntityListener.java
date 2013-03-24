ad22c63854f87f4176b849fa6ab593277d94b295
Jack Huey <kitskub@gmail.com>
2013-03-24 17:54:00 -0500

    Check projectiles

--- src/main/java/com/randude14/hungergames/listeners/EntityListener.java
+++ src/main/java/com/randude14/hungergames/listeners/EntityListener.java
@@ -10,8 +10,10 @@ import com.randude14.hungergames.stats.PlayerStat;
 import com.randude14.hungergames.stats.PlayerStat.PlayerState;
 import com.randude14.hungergames.stats.PlayerStat.Team;
 import com.randude14.hungergames.utils.ChatUtils;
+import org.bukkit.entity.Entity;
 
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -35,23 +37,24 @@ public class EntityListener implements Listener{
 				EntityDamageByEntityEvent newEvent = (EntityDamageByEntityEvent) event;
 				double period = Config.GRACE_PERIOD.getDouble(hurtGame.getSetup());
 				long startTime = hurtGame.getInitialStartTime();
+				Entity damager = newEvent.getDamager() instanceof Projectile ? ((Projectile) newEvent.getDamager()).getShooter() : newEvent.getDamager();
 				if (((System.currentTimeMillis() - startTime) / 1000) < period) {
 					event.setCancelled(true);
-					if (newEvent.getDamager() instanceof Player) {
-						ChatUtils.error((Player) newEvent.getDamager(), "You can't hurt that player during the grace-period!");
+					if (damager instanceof Player) {
+						ChatUtils.error((Player) damager, "You can't hurt that player during the grace-period!");
 					}
 				}
-				if (newEvent.getDamager() instanceof Player && hurtGame.contains((Player) newEvent.getDamager())) {
+				if (damager instanceof Player && hurtGame.contains((Player) damager)) {
 					if (!Config.TEAMS_ALLOW_FRIENDLY_DAMAGE.getBoolean(hurtGame.getSetup())) {
 						Team hurtTeam = hurtGame.getPlayerStat(player).getTeam();
 						if (hurtGame.getPlayerStat(player).getState() == PlayerState.WAITING) {
 							event.setCancelled(true);
 						} else if (hurtTeam != null){
-							Team hurterTeam = hurtGame.getPlayerStat((Player) newEvent.getDamager()).getTeam();
+							Team hurterTeam = hurtGame.getPlayerStat((Player) damager).getTeam();
 							if (hurterTeam != null) {
 								if (hurtTeam.getName().equals(hurterTeam.getName())) {
 									event.setCancelled(true);
-									ChatUtils.error((Player) newEvent.getDamager(), "You can't hurt a player on your team!");
+									ChatUtils.error((Player) damager, "You can't hurt a player on your team!");
 								}
 							}
 						}
